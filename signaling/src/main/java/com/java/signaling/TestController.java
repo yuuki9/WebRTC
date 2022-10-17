0073026d0fbd6595fb2d6eb9cced0c898f1beeac
EGIS-PC <wlsghtjd23@gmail.com>
2022-10-17 13:58:22 +0900

    test

--- signaling/src/main/java/com/java/signaling/TestController.java
+++ signaling/src/main/java/com/java/signaling/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@GetMapping("/index")
-	public String index() throws Exception {
-  	 return "index";
-	}
+	/*
+	 * @GetMapping("/index") public String index() throws Exception { return
+	 * "index"; }
+	 */
 }
